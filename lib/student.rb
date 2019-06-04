ea15ea82610623191319cb5ec190f6878a225b34
Kari O'Neal <triadfirewood@gmail.com>
2019-06-04 14:24:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,8 @@ class Student
       VALUES(?,?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    
+    @id = DB[:conn].execute("SELECT last_insert_row id() FROM students")[0][0]
   end
   
   
