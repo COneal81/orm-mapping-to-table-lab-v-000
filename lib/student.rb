e9bd1f9f660512b4b22f1e934eb4b679573bcbb7
Kari O'Neal <triadfirewood@gmail.com>
2019-06-04 13:59:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,13 @@
 class Student
+  
+  attr_reader :id 
+  
+  attr_accessor :name, :grade
+  
+  def initialize(name, grade)
+    @name = name 
+    @grade = grade
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
